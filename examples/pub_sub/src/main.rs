c2b82cb50fb5f0b781d3e7d4fc0983fd4ed9be1a
mc1098 <m.cripps1@uni.brighton.ac.uk>
2021-06-30 06:37:20 +0100

    Add simple pub_sub example

---
+++ examples/pub_sub/src/main.rs
@@ -0,0 +1,71 @@
+mod event_bus;
+mod producer;
+mod subscriber;
+
+use producer::Producer;
+use subscriber::Subscriber;
+use yew::{html, Component, ComponentLink, Html, ShouldRender};
+
+pub struct Model;
+
+impl Component for Model {
+    type Message = ();
+    type Properties = ();
+
+    fn create(_props: Self::Properties, _link: ComponentLink<Self>) -> Self {
+        Self
+    }
+
+    fn change(&mut self, _msg: Self::Properties) -> ShouldRender {
+        false
+    }
+
+    fn update(&mut self, _props: Self::Message) -> ShouldRender {
+        unimplemented!()
+    }
+
+    fn view(&self) -> Html {
+        html! {
+            <>
+                <Producer />
+                <Subscriber />
+            </>
+        }
+    }
+}
+
+fn main() {
+    yew::start_app::<Model>();
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+    use wasm_bindgen_test::*;
+    use yew::{virtual_dom::test_render, web_sys::HtmlButtonElement};
+    use yew_test::prelude::*;
+
+    wasm_bindgen_test_configure!(run_in_browser);
+
+    #[wasm_bindgen_test]
+    fn click_producer_and_view_subscriber_message() {
+        let rendered: TestRender = test_render(html! { <Model /> }).into();
+
+        // get subscriber heading message
+        let sub_message: HtmlElement = rendered
+            .get_by_aria_role(AriaRole::Heading, "No message yet.")
+            .expect("subscriber heading should have a default of 'No message yet.'");
+
+        // get producer button
+        let button: HtmlButtonElement = rendered
+            .get_by_aria_role(AriaRole::Button, "PRESS ME")
+            .unwrap();
+
+        // click the producer button which will send a message to the subscriber through an agent.
+        button.click();
+
+        // assert that the subscriber received the message and updated the heading.
+        assert_text_content!("Message received", sub_message);
+    }
+}
