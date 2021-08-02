bc6eb29e1c85d173c256ca1ce1f4d9a540ba952b
mc1098 <m.cripps1@uni.brighton.ac.uk>
2021-08-02 14:38:52 +0000

    Getter and setter properties with js_sys Reflect (#35)
    Instead of using manually built `wasm_bindgen` functions, we can just use `js_sys::Reflect` to get or set properties when the value is an `Object`. This reduces some clutter and avoids using js directly.

--- crates/sap-utils/src/html.rs
+++ crates/sap-utils/src/html.rs
@@ -3,23 +3,26 @@ use web_sys::Element;
 
 #[wasm_bindgen(module = "/js/sap-utils.js")]
 extern "C" {
-    fn get_value(element: &Element) -> JsValue;
-    fn set_value(element: &Element, value: JsValue) -> JsValue;
     fn format(str: JsValue) -> JsValue;
 }
 
-pub fn get_element_value<T: JsCast>(element: &T) -> Option<String> {
-    element
-        .dyn_ref::<Element>()
-        .and_then(|e| get_value(e).as_string())
+macro_rules! get_js_property_impl {
+    ($getter:ident, $setter:ident, $property_name:literal) => {
+        pub fn $getter<T: JsCast>(element: &T) -> Option<String> {
+            js_sys::Reflect::get(&element.into(), &$property_name.into())
+                .ok()
+                .and_then(|v| v.as_string())
+        }
+
+        pub fn $setter<T: JsCast>(element: &T, value: &str) -> bool {
+            js_sys::Reflect::set(&element.into(), &$property_name.into(), &value.into())
+                .expect("implementations of JsCast should be Objects")
+        }
+    };
 }
 
-pub fn set_element_value<T: JsCast>(element: &T, value: &str) -> bool {
-    if let Some(element) = element.dyn_ref::<Element>() {
-        JsValue::TRUE == set_value(element, value.into())
-    } else {
-        false
-    }
+get_js_property_impl! {
+    get_element_value, set_element_value, "value"
 }
 
 pub fn format_html(html: &str) -> String {
