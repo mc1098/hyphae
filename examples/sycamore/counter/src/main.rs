335bef4c49e044a2eb48b089ab77cc58676e0e16
mc1098 <m.cripps1@uni.brighton.ac.uk>
2021-12-16 03:36:05 +0000

    Update example versions (#84)
    * Update to Sycamore 0.7
    
    * Update to Yew 0.19.0

--- examples/sycamore/counter/src/main.rs
+++ examples/sycamore/counter/src/main.rs
@@ -1,7 +1,7 @@
 use sycamore::prelude::*;
 
 #[component(App<G>)]
-fn app() -> Template<G> {
+fn app() -> View<G> {
     let counter = Signal::new(0);
 
     create_effect(cloned!((counter) => move || {
@@ -12,7 +12,7 @@ fn app() -> Template<G> {
 
     let reset = cloned!((counter) => move |_| counter.set(0));
 
-    template! {
+    view! {
         div {
             "Counter demo"
             p(class="value") {
@@ -30,7 +30,7 @@ fn app() -> Template<G> {
 }
 
 fn main() {
-    sycamore::render(|| template! { App() });
+    sycamore::render(|| view! { App() });
 }
 
 #[cfg(test)]
@@ -39,13 +39,14 @@ mod tests {
     use sap::prelude::*;
     use wasm_bindgen_test::*;
     wasm_bindgen_test_configure!(run_in_browser);
+
     use super::*;
     use web_sys::{HtmlButtonElement, HtmlElement};
 
     #[wasm_bindgen_test]
     fn can_count_and_reset() {
         let rendered = QueryElement::new();
-        sycamore::render_to(|| template! { App() }, &rendered);
+        sycamore::render_to(|| view! { App() }, &rendered);
 
         let inc_btn: HtmlButtonElement = rendered.assert_by_text("Increment");
         let counter: HtmlElement = rendered.assert_by_text("Value: 0");
